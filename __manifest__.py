2cbb78da27a4442733a0dd84b8ff9e726e13dc7f
lukjasin <luk.jasin@gmail.com>
2024-06-13 11:51:55 +0200

    structure

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Real Estate Ads",
+    "version": "1.0",
+    "website": "https://www.portacapena.com",
+    "author": "Kimbo",
+    "description": """
+        Real estate modul to show available properties.
+    """,
+    "category": "Sales",
+    "depends": [],
+    "data": [],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3"
+}
